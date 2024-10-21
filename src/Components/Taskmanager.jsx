40d4638eb1dbc23d582f41c6ee37f6a64f170cf6
Josef <Horizon.dawn67@gmail.com>
2024-10-22 01:50:13 +0300

    1.0 poor design

--- src/Components/Taskmanager.jsx
+++ src/Components/Taskmanager.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/style.css';
 
 const Taskmanager = () => {
 
-    let task_list = [];
+    let [task_list, set_task_list] = useState([]);
     let choice;
     let taskName;
     let checkbox_color;
 
+    // Вывод имени из хранилища
     window.onload = function() {
         let name = document.getElementById('gret');
 
@@ -16,6 +17,7 @@ const Taskmanager = () => {
                 name.value = localStorage.getItem(localStorage.key(i));
             }
         }
+        // Запись в массив task_list всех значений из хранилища кроме имени
     }
     for (let i = 0; i < localStorage.length; i++) {
         if(localStorage.key(i) === 'name') {
@@ -46,17 +48,32 @@ const Taskmanager = () => {
     }
     // Запись task'а в localStorage
     function onTaskClick () {
-        localStorage.setItem(Math.floor(Math.random() * 10000000) + " " + choice, taskName);
+        let new_task_id = Math.floor(Math.random() * 10000000) + " " + choice;
+        set_task_list(task_list = [])
+        localStorage.setItem(new_task_id, taskName);
     }
     // Удаление task'а
     function onDeleteClick (event) {
         localStorage.removeItem(event.target.id);
+        set_task_list(task_list = []);
     }
     // Редактирование task'а
     function onEditClick (event) {
-        if (event.target.className === "task_div_edit_but") {
-            console.log(event.target.closest("input"));
-        }
+        let neededElement = document.getElementById(event.target.id + "_");
+        neededElement.value = neededElement.placeholder
+        neededElement.disabled = false;
+        neededElement.focus();
+    }
+
+    function onSaveBlur(event) {
+        localStorage.setItem(event.target.id.split('_')[0], event.target.value)
+    }
+
+    function deleteAll () {
+        let name = document.getElementById('gret').value
+        localStorage.clear()
+        localStorage.setItem('name', name)
+        set_task_list(task_list = [])
     }
 
     return(
@@ -89,7 +106,7 @@ const Taskmanager = () => {
                 <div className="taskmanager_list">
                     <div className="taskmanager_list_header">
                         <p>Список дел</p>
-                        <button>Удалить всё</button>
+                        <button onClick={deleteAll}>Удалить всё</button>
                     </div>
                     <div className="taskmanager_list_body">
                         {
@@ -101,22 +118,22 @@ const Taskmanager = () => {
                                     checkbox_color = "checkbox_purple";
                                 }
                                 return(
-                                    <div className="task_div" onClick={onEditClick}>
-                                        <div className="task_div_checkbox">
-                                            <label className={checkbox_color}>
-                                                <input type="checkbox" className="checkbox"/>
-                                                <span></span>
-                                            </label>                                    
-                                        </div>
-                                        <div className="task_div_name">
-                                            <input type="text" placeholder={task.split("_")[0]}/>
-                                        </div>
-                                        <div className="task_div_edit">
-                                            <button className="task_div_edit_but" id={task.split("_")[1]}>Редактировать</button>
-                                        </div>
-                                        <div className="task_div_delete">
+                                    <div className="task_div">
+                                        {/*<div className="task_div_checkbox">*/}
+                                        {/*    <label className={checkbox_color}>*/}
+                                        {/*        <input type="checkbox" className="checkbox"/>*/}
+                                        {/*        <span></span>*/}
+                                        {/*    </label>                                    */}
+                                        {/*</div>*/}
+                                        {/*<div className="task_div_name">*/}
+                                            <input type="text" className="task_input_value" disabled onBlur={onSaveBlur} id={task.split("_")[1] + "_"} placeholder={task.split("_")[0]}/>
+                                        {/*</div>*/}
+                                        {/*<div className="task_div_edit">*/}
+                                            <button className="task_div_edit_but" id={task.split("_")[1]} onClick={onEditClick}>Редактировать</button>
+                                        {/*</div>*/}
+                                        {/*<div className="task_div_delete">*/}
                                             <button className="task_div_delete_but" onClick={onDeleteClick} id={task.split("_")[1]}>Удалить</button>
-                                        </div>
+                                        {/*</div>*/}
                                     </div>
                                 )
                             })
